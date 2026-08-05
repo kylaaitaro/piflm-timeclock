195b26bd7cb8900dba79e0787f202b8f87478b15
kylaaitaro <kylaaitaro@gmail.com>
2026-08-05 15:24:58 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GOOGLE_FORM_URL = "https://forms.gle/PyhhLqpccmwXALpMA";
+const GOOGLE_FORM_URL = "https://script.google.com/macros/s/AKfycbwBkK_gadhT-nEJossjfjr52oOYmU2E9C1JoRBsGW1Ogn6nhx-v5vlVLCPFyu2XxXEV1Q/exec";
