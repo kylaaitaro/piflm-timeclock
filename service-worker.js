f56ab36afac8e39f442d9563fa23885842453997
kylaaitaro <kylaaitaro@gmail.com>
2026-08-05 15:54:06 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,68 @@
+const CACHE_NAME = "piflm-timeclock-v2";
 
-const CACHE_NAME="piflm-timeclock-v1";
-const ASSETS=["./","./index.html","./styles.css","./config.js","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./config.js",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener(
+  "install",
+  (event) => {
+    self.skipWaiting();
+
+    event.waitUntil(
+      caches
+        .open(CACHE_NAME)
+        .then((cache) => {
+          return cache.addAll(ASSETS);
+        })
+    );
+  }
+);
+
+self.addEventListener(
+  "activate",
+  (event) => {
+    event.waitUntil(
+      Promise.all([
+        self.clients.claim(),
+
+        caches
+          .keys()
+          .then((keys) => {
+            return Promise.all(
+              keys
+                .filter(
+                  (key) =>
+                    key !== CACHE_NAME
+                )
+                .map(
+                  (key) =>
+                    caches.delete(key)
+                )
+            );
+          })
+      ])
+    );
+  }
+);
+
+self.addEventListener(
+  "fetch",
+  (event) => {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => {
+          return caches.match(
+            event.request
+          );
+        })
+    );
+  }
+);
